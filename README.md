beaa7b3f844bc0df067054f0244c5dc21b38b57a
Citrinae-Lime <85402382+Citrinae-Lime@users.noreply.github.com>
2022-04-24 20:34:35 +0200

    更改命名规则

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FFmpeg-AudioVisualization
 
 这些是使用FFmpeg创建音频可视化的PowerShell脚本，
-可视化使用的过滤器可以在`[]`中查看。  
+脚本以可视化使用的过滤器命名。  
 ~~脚本中出现的英语均为机翻。~~
 
 使用效果可在 [我的B站账号](https://space.bilibili.com/5677062/) 中查看。
--- ShowFreqs.ps1
+++ ShowFreqs.ps1
@@ -15,9 +15,9 @@ ffmpeg.exe -hide_banner -i $p -i $a `
 -filter_complex "[0]scale=1:1:flags=neighbor,scale=$width`:$height,setsar=1:1[bg],
                  [0]scale=$UI_WH`:$UI_WH,pad=w=iw+$($width/10):color=random@0x00[cover],
                  [1:a]showfreqs=s=$UI_WH`x$UI_WH`:ascale=sqrt:colors=F3F3F3|F5F5F5:averaging=15:fscale=log,
-                 drawtext=fontcolor=F4F4F4:fontfile=$font`:fontsize=90:text=$title[freq];
+                 drawtext=fontcolor=F4F4F4:fontfile=$font`:fontsize=86:text=$title[freq];
                  [cover][freq]hstack[ui];
                  [bg][ui]overlay=x=(W-w)/2:y=(H-h)/2[v]" `
--map '[v]' -map '1:a' -c:a libopus -c:v libvpx-vp9 -shortest Freqs_A2V.mkv
+-map '[v]' -map '1:a' -c:a libopus -c:v libvpx-vp9 -shortest Freqs_A2V.webm
 
 Read-Host -Prompt 'Press Enter to exit'
