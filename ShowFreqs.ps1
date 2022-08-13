f421def457d0827050575f4b8af2cf8c982a3b64
Citrinae-Lime <85402382+Citrinae-Lime@users.noreply.github.com>
2022-08-13 23:29:33 +0200

    Add bg color

--- ShowFreqs.ps1
+++ ShowFreqs.ps1
@@ -7,13 +7,14 @@ $p = "input -f null"
 $width = 2560
 $height = [int]$width*0.5625  # 16:9; 0.75 for 4:3
 $UI_WH = [int]$width/2*0.8
+$bg_color = "random"
 # Title
 $font = font.ttf
 $title = "A2V by FFmpeg"
 
 ffmpeg.exe -hide_banner -i $p -i $a `
--filter_complex "[0]scale=1:1:flags=neighbor,scale=$width`:$height,setsar=1:1[bg],
-                 [0]scale=$UI_WH`:$UI_WH,pad=w=iw+$($width/10):color=random@0x00[cover],
+-filter_complex "color=c=$bg_color`:s=$width`x$height`:r=60[bg],
+                 [0]scale=$UI_WH`:$UI_WH,pad=w=iw+$($width/10):color=$bg_color[cover],
                  [1:a]showfreqs=s=$UI_WH`x$UI_WH`:ascale=sqrt:colors=F3F3F3|F5F5F5:averaging=15:fscale=log,
                  drawtext=fontcolor=F4F4F4:fontfile=$font`:fontsize=86:text=$title[freq];
                  [cover][freq]hstack[ui];
