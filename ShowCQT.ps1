61cbce0a12a4a1a18f9761181db5832312eb52de
Citrinae-Lime <85402382+Citrinae-Lime@users.noreply.github.com>
2022-10-31 10:27:53 +0100

    Stream copy on audio

--- ShowCQT.ps1
+++ ShowCQT.ps1
@@ -19,19 +19,19 @@ $audio = $(Read-Host -Prompt 'Audio file') -replace '"',''
 $filter = "[1:a]showcqt=s=$CQT_width`x$ih`:bar_g=2:sono_g=2:tc=0.5[vcqt],
            [0]$scale[vcqt]hstack=shortest=1[vo]"
 
-# Is 256kbps@opus and vp9 in webm container, you can change it if you want.
+# Is audio copy and vp9 in mkv container, you can change it if you want.
 # Check the extension replacement rule!
 # For folder, please add a \ after the path.
 
 if ($audio -match '\\$') {
     Set-Location $audio
     foreach ($audio_file in Get-ChildItem -Exclude cover.*) {
-        $output_file = $audio_file -replace '.flac|.wav|.mp3|.m4a','.webm'
-        ffmpeg.exe -hide_banner -loop 1 -i $image -i $audio_file -filter_complex $filter -map '[vo]' -map '1:a' -c:a libopus -b:a 256k -c:v libvpx-vp9 $output_file
+        $output_file = $audio_file -replace '.flac|.wav|.mp3|.m4a','.mkv'
+        ffmpeg.exe -hide_banner -loop 1 -i $image -i $audio_file -filter_complex $filter -map '[vo]' -map '1:a' -c:a copy -c:v libvpx-vp9 $output_file
     }
 }
 else {
-    $output_file = $audio -replace '.flac|.wav|.mp3|.m4a','.webm'
-    ffmpeg.exe -hide_banner -loop 1 -i $image -i $audio -filter_complex $filter -map '[vo]' -map '1:a' -c:a libopus -b:a 256k -c:v libvpx-vp9 $output_file
+    $output_file = $audio -replace '.flac|.wav|.mp3|.m4a','.mkv'
+    ffmpeg.exe -hide_banner -loop 1 -i $image -i $audio -filter_complex $filter -map '[vo]' -map '1:a' -c:a copy -c:v libvpx-vp9 $output_file
 }
 Read-Host -Prompt 'Press Enter to exit'
