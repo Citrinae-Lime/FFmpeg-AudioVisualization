22bf0d5ae4f93f1db5b1d3f04b1a687bf59f6996
Citrinae-Lime <85402382+Citrinae-Lime@users.noreply.github.com>
2023-03-03 17:32:59 +0100

    use UI_color

--- ShowFreqs.ps1
+++ ShowFreqs.ps1
@@ -7,28 +7,23 @@ $p = "input -f null"
 add-type -AssemblyName System.Drawing
 # Output resolution
 $width = 2560
-$height = [int]$width*0.5625  # 16:9; 0.75 for 4:3
-$UI_WH = [int]$width/2*0.8
+$height = [int]($width*0.5625)  # 16:9; 0.75 for 4:3
+$UI_WH = [int]($width/2*0.8)
 $bg_color = [System.Drawing.Color]::FromArgb(36, 41, 46)
 # Title
-$font = "font.ttf"
-$title = "A2V by FFmpeg"
+$font = "Nexa.otf"
+$title = "A2V use FFmpeg&showfreqs"
 
-function IsLightColor($color){
-    $relativeLuminance = 0.2126*$bg_color.R + 0.7152*$bg_color.G + 0.0722*$bg_color.B
-    if ($relativeLuminance -ge 0.5){return $true}
-    else {return $false}
-}
-
-if (IsLightColor $bg_color){$UI_color = "0x171717"}
+$relativeLuminance = 0.2126*$bg_color.R + 0.7152*$bg_color.G + 0.0722*$bg_color.B
+if ($relativeLuminance -ge 0.5){$UI_color = "0x171717"}
 else {$UI_color = "0xF4F4F4"}
 
 ffmpeg.exe -hide_banner -i $p -i $a `
 -filter_complex `
-"color=c=$bg_color.name`:s=$width`x$height`:r=60[bg],
- [0]scale=$UI_WH`:$UI_WH,pad=w=iw+$($width/10):color=$bg_color.name[cover],
- [1:a]showfreqs=s=$UI_WH`x$UI_WH`:ascale=sqrt:colors=F3F3F3|F5F5F5:averaging=15:fscale=log,
-      drawtext=fontcolor=F4F4F4:fontfile=$font`:fontsize=h/10:text=$title[freq];
+"color=c=$($bg_color.name):s=$width x$height :r=60[bg],
+ [0]scale=$UI_WH :$UI_WH,pad=w=iw+$($width/10):color=$($bg_color.name)[cover],
+ [1:a]showfreqs=s=$UI_WH x$UI_WH :ascale=sqrt:colors=$UI_color|$UI_color :averaging=15:fscale=log,
+      drawtext=fontcolor=$UI_color :fontfile=$font :fontsize=h/10:text=$title[freq];
  [cover][freq]hstack[ui];
  [bg][ui]overlay=x=(W-w)/2:y=(H-h)/2[v]" `
 -map '[v]' -map '1:a' -c:a copy -c:v hevc_qsv -preset:v 3 -global_quality 10 -look_ahead 1 -scenario 3 -shortest Freqs_A2V.mkv
