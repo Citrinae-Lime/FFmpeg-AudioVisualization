bd5d1202aa121404a798ba92c364589b40393d46
Citrinae-Lime <Citrinae-Lime@users.noreply.github.com>
2024-01-10 22:55:11 +0100

    重构 Freq
    添加了帮助注释，
    重构了输入部分

--- ShowFreqs.ps1
+++ ShowFreqs.ps1
@@ -1,17 +1,53 @@
-﻿# 将 "input -f null" 替换为你的文件路径。
-# 音频路径
-$a = "input -f null"
-# 图像路径
-$p = "input -f null"
+﻿<#
+  .SYNOPSIS
+  创建基于 ShowFreqs 的音频可视化。
 
-# 输出分辨率
-$width = 2560
+  .PARAMETER InputAudio
+  输入音频，支持所有 FFmpeg 所支持的格式。
+
+  .PARAMETER InputImage
+  输入图像，支持所有 FFmpeg 所支持的格式。
+
+  .PARAMETER OutputVideo
+  输出媒体，支持所有 FFmpeg 所支持的格式。
+  默认使用原文件名.mkv。
+
+  .PARAMETER title
+  显示在可视化上方的标题，默认从标签获取。
+  特殊字符需转义。
+
+  .PARAMETER font
+  指定标题的字体文件。
+  默认 Times New Roman。
+
+  .PARAMETER width
+  视频宽度。
+  默认 2560，高度根据比率（默认16:9）计算得出。
+
+  .EXAMPLE
+  .\ShowFreqs.ps1 "D:\Path to\flie.wav" "C:\Path\to cover.jpeg"
+
+  .EXAMPLE
+  .\ShowFreqs.ps1 -a "D:\Path to\音频.flac" -p "C:\Path\to cover.png" -title "A2V by FFmpeg" -font Nexa.otf -width 1920
+#>
+
+[CmdletBinding()]
+Param(   
+    [Parameter(Mandatory=$True)][ValidateNotNullOrEmpty()][Alias("a")][String]$InputAudio,
+    [Parameter(Mandatory=$True)][ValidateNotNullOrEmpty()][Alias("p")][String]$InputImage,
+    [Parameter(Mandatory=$False)][Alias("o")][String]$OutputVideo,
+
+    [Parameter(Mandatory=$False)][AllowEmptyString()][String]$title,
+    [Parameter(Mandatory=$False)][String]$font = "$env:windir\Fonts\times.ttf",
+    [Parameter(Mandatory=$False)][ValidateNotNull()][Int]$width = 2560
+    )
+
+# 计算输出分辨率
 $height = [int]($width*0.5625)  # 16:9; 0.75 for 4:3
 $UI_WH = [int]($width/2*0.8)
-# 标题（需指定字体文件，特殊字符需转义）
-$font = "$env:windir\Fonts\times.ttf" 
-$title = "" # 留空则从标签获取
-if ($title -eq "") {$title = ffprobe.exe -v error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 $a}
+
+if ($OutputVideo -eq $null) {$OutputVideo = $InputAudio -replace '\.\w+$','.mkv'}
+if ($title -eq $null) {$title = ffprobe.exe -v error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 $InputAudio}
 
 $filters = @"
 [0]
@@ -52,10 +88,9 @@ $filters = @"
         y = (H-h)/2
 [v]
 "@
-$o = $a -replace '.flac|.wav|.mp3|.m4a','.mkv'
 
-ffmpeg.exe -hide_banner -i $p -i $a `
+ffmpeg.exe -hide_banner -i $InputImage -i $InputAudio `
 -filter_complex $filters -map '[v]' -map '1:a' `
--c:a copy -c:v librav1e -shortest $o
+-c:a copy -c:v librav1e -shortest $OutputVideo
 
 Read-Host -Prompt '按任意键退出'
